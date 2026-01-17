39b9559c925e2e3a407bcf28c79a337008b6d450
Jack <jckwind11@gmail.com>
2026-01-16 22:41:57 -0500

    .

--- tests/e2e.rs
+++ tests/e2e.rs
@@ -6,8 +6,6 @@ mod e2e_basic;
 mod e2e_batching;
 #[path = "e2e/e2e_client.rs"]
 mod e2e_client;
-#[path = "e2e/e2e_concurrency_bug.rs"]
-mod e2e_concurrency_bug;
 #[path = "e2e/e2e_multimodal.rs"]
 mod e2e_multimodal;
 #[path = "e2e/e2e_stop_sequences.rs"]
